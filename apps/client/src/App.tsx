a4046c2b4727c7ad63a61a547da1224b85693058
Abel Gonzalez <racklif.abel.gm@gmail.com>
2022-11-04 00:39:30 -0400

    code

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,39 +1,33 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-const client = new W3CWebSocket("ws://localhost:4000");
-
-client.onopen = () => {
-  console.log("Connected");
-};
-
-client.onmessage = (message) => {
-  console.log(message);
-};
-
-client.close();
-
 function App() {
   const [count, setCount] = useState(0);
+  const client = useRef<W3CWebSocket>();
+
+  useEffect(() => {
+    client.current = new W3CWebSocket("ws://localhost:4000");
+
+    return () => {
+      if (client.current !== null) {
+        client?.current?.close();
+      }
+    };
+  }, []);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
+        <a href="https://vitejs.dev" target="_blank"></a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => client?.current?.send("add")}>ADD</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,17 +4,25 @@ import WebSocket, { WebSocketServer } from "ws";
 const server = createServer();
 const wss = new WebSocketServer({ server });
 
+let score = 0;
+
 wss.on("connection", function connection(ws) {
-  ws.on("message", function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+  wss.clients.forEach((client) => {
+    client.send(score);
   });
 
+  ws.on("message", function (data) {
+    console.log(data);
+    score++;
+    wss.clients.forEach((client) => {
+      client.send(score);
+    });
+  });
+});
+
+wss.on("close", function () {
   wss.clients.forEach((client) => {
-    client.send("message");
+    client.send("disconnected");
   });
 });
 
